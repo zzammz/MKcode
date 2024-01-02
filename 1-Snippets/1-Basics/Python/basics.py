c78f4197352b9224515de050724a9c0094c327b6
Zzamm Z <56573737+zzammz@users.noreply.github.com>
2024-01-01 16:54:26 -0800

    Add files via upload

--- 1-Snippets/1-Basics/Python/basics.py
+++ 1-Snippets/1-Basics/Python/basics.py
@@ -751,6 +751,114 @@ def TEST_enum():
         print ("idx:{}  ch:{}".format(idx, ch))
     return
 
+# ------------------
+def PrintMe(p_msg):
+    print ("\n... " + p_msg + "()")
+    return
+
+def FindDupes (p_array):
+    PrintMe("FindDupes")
+    dupe_response = -1
+    ictr = 0
+    jctr = 0
+    len_array = len(p_array)
+    for ictr in range (0, len_array-1):
+        for jctr in range (ictr+1, len_array):
+            if p_array[ictr] == p_array[jctr]:
+                dupe_response = p_array[ictr]
+                return dupe_response
+
+    return dupe_response
+
+
+def FindDupes2 (p_array):
+    PrintMe("FindDupes2")
+    p_array.sort()
+    len_array = len(p_array)
+    if len_array <= 1:
+        return -1
+    ictr = 0
+    for ictr in range (0, len_array-1, 1):
+        if p_array[ictr] == p_array[ictr+1]:
+            return p_array[ictr]
+    return -1
+
+
+def TEST_FindDupes():
+    test_array = [11, 13, 7, 11, 1, 3, 8, 4]
+    response_dupe_check = FindDupes(test_array)
+    if response_dupe_check == -1:
+        print("No duplicates were found")
+    else:
+        print("Duplicate number: " + str(response_dupe_check))
+
+    response_dupe_check = FindDupes2(test_array)
+    if response_dupe_check == -1:
+        print("No duplicates were found")
+    else:
+        print("Duplicate number: " + str(response_dupe_check))
+
+    return
+
+def MoviesOnFlight(p_array, p_duration, p_max_gap):
+    PrintMe("MoviesOnFlight")
+    p_array.sort(reverse=True)
+    for ictr in range (0, len(p_array)-1, 1):
+        time_spent = p_duration - p_array[ictr] - p_array[ictr+1]
+        if time_spent > 0 and time_spent <= p_max_gap:
+            print(p_duration - p_array[ictr] - p_array[ictr+1])
+            print ("[" + str(p_array[ictr]) + ", " + str(p_array[ictr+1]) + "]")
+            return
+
+    print ("Could not find 2 movies to watch within the " + str(p_duration) + " time period")
+    return
+
+
+def TEST_MoviesOnFlight():
+    movie_array = [90, 85, 75, 60, 120, 150, 125]
+    flight_duration = 250
+    max_gap_allowed = 30
+    MoviesOnFlight(movie_array, flight_duration, max_gap_allowed)
+    return
+
+def PushZeroesToEnd(p_array):
+    PrintMe("PushZeroesToEnd")
+    print ("input array: " + str(p_array))
+    len_array = len(p_array)
+    for ictr in range (0, len_array, 1):
+        if(p_array[ictr] == 0):
+            for jctr in range (ictr, len_array-1, 1):
+                p_array[jctr] = p_array[jctr+1]
+            p_array[jctr+1] = 0
+    print("output array: " + str(p_array))
+    return
+
+def TEST_PushZeroestoEnd():
+    test_array = [1, 2, 0, 4, 3, 0, 5, 0]
+    #test_array = [1, 0, 3, 5, 0]
+    PushZeroesToEnd(test_array)
+    return
+
+def TEST_dict():
+    mydict = {
+        "brand" : "Ford",
+        "model" : "Mustabg",
+        "year" : 1964,
+        "type" : {
+                "speed" : "Four",
+                "race" : "Shelby"
+        }
+    }
+    print (mydict)
+    xx = mydict["model"]
+    print (xx)
+    mydict["color"] = "red"
+    mydict["year"] = 2019
+    print(mydict)
+    for (x,y) in mydict.items():
+        print(x, y)
+    return
+
 
 # ------ main
 #check_time_spent()
@@ -800,5 +908,11 @@ print ("Hello, World!\n")
 
 #examples_path()
 examples_commandline()
+
+
+#TEST_FindDupes()
+#TEST_MoviesOnFlight()
+#TEST_PushZeroestoEnd()
+#TEST_dict()
 
 
