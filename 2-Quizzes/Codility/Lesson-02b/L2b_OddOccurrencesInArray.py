007c55fe8c6e94c37fbec17534f77272549b2823
Zzamm Z <56573737+zzammz@users.noreply.github.com>
2024-01-02 09:11:46 -0800

    Update L2b_OddOccurrencesInArray.py

--- 2-Quizzes/Codility/Lesson-02b/L2b_OddOccurrencesInArray.py
+++ 2-Quizzes/Codility/Lesson-02b/L2b_OddOccurrencesInArray.py
@@ -1,1 +1,41 @@
+# ==========
+def L2b_OddOccurrencesInArray (p_array):
+    p_array.sort()
+    ictr = 1
+    item_count = 1
+    prev_item = p_array[0]
+
+    while ictr < len(p_array):
+        this_item = p_array[ictr]
+        #print ("[{}]: prev={}  this={}  count={}".format(ictr, prev_item, this_item, item_count))
+
+        if this_item != prev_item:
+            if item_count == 1:
+                return prev_item
+            item_count = 1
+            prev_item = this_item
+            ictr += 1
+            continue
+        else:
+            prev_item = this_item
+            item_count += 1
+            ictr += 1
+            continue
+
+    return prev_item
+
+def TEST_L2b_OddOccurrencesInArray():
+    array_1 = [5, 8, 8, 5, 7, 3, 4, 3, 4]
+    print ("array={}  odd number={}".format(array_1, L2b_OddOccurrencesInArray(array_1)))
+
+    array_1 = [5, 7, 8, 5, 7, 3, 4, 3, 4]
+    print ("array={}  odd number={}".format(array_1, L2b_OddOccurrencesInArray(array_1)))
+
+    array_1 = [5, 2, 7, 7, 5, 7, 3, 4, 3, 4]
+    print ("array={}  odd number={}".format(array_1, L2b_OddOccurrencesInArray(array_1)))
+
+    return
+
+# =========== main
+TEST_L2b_OddOccurrencesInArray()
 
