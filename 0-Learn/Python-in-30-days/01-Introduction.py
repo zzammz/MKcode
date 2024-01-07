785218dd645beb7933f9903369c17144eba44064
Zzamm Z <56573737+zzammz@users.noreply.github.com>
2024-01-07 09:14:13 -0800

    Update 01-Introduction.py

--- 0-Learn/Python-in-30-days/01-Introduction.py
+++ 0-Learn/Python-in-30-days/01-Introduction.py
@@ -16,6 +16,6 @@ print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
 print(type('Asabeneh'))          # String
 print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
+print(type({'name':'Jim'})) # Dictionary
 print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
