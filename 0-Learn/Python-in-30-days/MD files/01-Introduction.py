37c46047a09af78ae0d52423ed1e8446fee9b09b
Zzamm Z <56573737+zzammz@users.noreply.github.com>
2024-01-29 13:21:58 -0800

    Update 01-Introduction.py

--- 0-Learn/Python-in-30-days/MD files/01-Introduction.py
+++ 0-Learn/Python-in-30-days/MD files/01-Introduction.py
@@ -9,7 +9,7 @@ print(3 ** 2)  # exponential(**)
 print(3 % 2)   # modulus(%)
 print(3 // 2)  # Floor division operator(//)
 
-# Checking data types
+# Checking data types 
 
 print(type(10))                  # Int
 print(type(3.14))                # Float
